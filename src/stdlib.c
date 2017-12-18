3ddf07f0436d490204cce8e2efc6fde66cb6198c
viezzo <aft2115@columbia.edu>
2017-12-18 02:47:07 -0500

    added support for \t

--- src/stdlib.c
+++ src/stdlib.c
@@ -8,8 +8,11 @@ void print(char *s){
   int i = 0;
   while (i < strlen(s)){
     c = s[i];
-    if (c == '\\' && s[i+1] == 'n'){
-      printf("\n");
+    if (c == '\\'){
+      if (s[i+1] == 'n')
+        printf("\n");
+      if (s[i+1] == 't')
+        printf("\t");
       i++;
     }
     else if (c == '\\' && s[i+1] == 't'){
@@ -28,8 +31,11 @@ void println(char *s){
   int i = 0;
   while (i < strlen(s)){
     c = s[i];
-    if (c == '\\' && s[i+1] == 'n'){
-      printf("\n");
+    if (c == '\\'){
+      if (s[i+1] == 'n')
+        printf("\n");
+      if (s[i+1] == 't')
+        printf("\t");
       i++;
     }
     else
