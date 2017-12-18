bdaf6acd7c94e6e64f3f94c098fbf0d0b6e53aaa
Jordan Lee <jordanlee133@gmail.com>
2017-12-18 15:01:07 -0500

    trying to figure out var

--- src/codegen.ml
+++ src/codegen.ml
@@ -189,7 +189,7 @@ else *)
       )
   | A.Call("println", [s]) ->
     let test = s in (match s with
-          A.StringLit test -> L.build_call println_func [| (expr builder s) |] "print" builder
+          A.StringLit test -> L.build_call println_func [| (expr builder s) |] "println" builder
         | _ -> L.build_call printf_func [| int_format_str_ln ; (expr builder s) |] "print" builder
       )
   (* | A.Call("sprintf", [s]) ->
