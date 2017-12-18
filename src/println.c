281604e77537e0cb2b2eef5dfe0d5b7fbce8902d
viezzo <aft2115@columbia.edu>
2017-12-18 02:20:30 -0500

    print

--- src/println.c
+++ src/println.c
@@ -2,7 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-void println(char *s)
-{
-  printf("%s\r\n", s);
+// Prints with \n escape character
+void print(char *s){
+  char c;
+  int i = 0;
+  while (i < strlen(s)){
+    c = s[i];
+    if (c == '\\' && s[i+1] == 'n'){
+      printf("\n");
+      i++;
+    }
+    else
+      printf("%c", c);
+    i++;
+  }
+}
+
+// as above, but with a newline at end
+void println(char *s){
+  char c;
+  int i = 0;
+  while (i < strlen(s)){
+    c = s[i];
+    if (c == '\\' && s[i+1] == 'n'){
+      printf("\n");
+      i++;
+    }
+    else
+      printf("%c", c);
+    i++;
+  }
+  printf("\n");
 }
