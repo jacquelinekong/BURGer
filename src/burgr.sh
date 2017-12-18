0bf744b4aa714c871f954455da4e2c8a1703bcdb
Jordan Lee <jordanlee133@gmail.com>
2017-12-18 00:10:32 -0500

    updated burgr.sh and old-burgr.sh

--- src/burgr.sh
+++ src/burgr.sh
@@ -2,14 +2,13 @@
 # usage: "./burgr.sh file-name"
 # do not append with .bun
 
+echo "------compiling object file------"
+gcc -c -Wall println.c
+echo "------running burger.native------"
 ./burger.native < tests/$1.bun > $1.ll
-clang $1.ll
-echo "--------------------"
-echo "Output for .ll test:"
-echo "--------------------"
-cat $1.ll
-echo "--------------------"
-echo "Running the executable"
-echo "--------------------"
-./a.out
-echo " "
+echo "------llc command------"
+llc $1.ll
+echo "------linking files------"
+gcc -o $1 $1.s println.o
+echo "------running executable------"
+./$1
